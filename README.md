9fc14e3cb4e30f764f0f18b147c6190deb3793a9
maxime vincent <maxime.vincent1@u-psud.fr>
2024-02-04 10:53:45 +0100

    Prototypage du solveur en Julia

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# swim_6h
+# Les 6 heures de nage
+Les « 6 heures de nage » sont une compétition amicale de nage avec palmes 
+se déroulant à la piscine d'Étampes. Elle est découpée en 12 relais de 30 minutes 
+chacun. Plusieurs équipes s'affrontent afin de réaliser la distance cumulée la 
+plus importante. Les bipalmes sont plus fréquemment utilisées mais les monopalmes 
+sont également autorisées.
+
+Chaque équipe est donc composée de 12 participants. Chaque participant donne
+ses disponibilités, c'est-à-dire les créneaux où il est disponible pour nager
+entre 13h00 et 19h00. Le but est de concilier ces disponibilités pour avoir
+un seul nageur par créneau. On cherche à maximiser le nombre de nageurs en lice
+tout en n'en gardant que 12. Si on a moins de 12 participants, certains devront
+nager au moins 2 fois, mais ils risquent d'être moins performants du fait de la 
+fatigue. Si on a plus de 12 participants, il faudra faire des choix et en éliminer 
+(les éliminés pourront éventuellement intégrer une autre équipe...).
+
+# Modélisation du problème
+Pour résoudre ce problème automatiquement on se propose de le modéliser à l'aide
+d'un programme d'optimisation sous contraintes en variables entières et plus
+précisément binaires.
+
+# Implémentation
+Le programme est prototypé en Julia à l'aide des librairies `Cbc` et `JuMP`.
+Une autre version en Python sera également réalisée à l'aide de la librairie `PuLP`
+et disposera d'une interface graphique client léger utilisant `Flask`.
+
+# Installation & Exécution
+## Julia
+```shell
+$ cd jl/
+$ julia
+julia> import Pkg
+julia> Pkg.add("Cbc")
+julia> Pkg.add("JuMP")
+$ julia planning.jl
+```
---
+++ jl/planning.jl
@@ -0,0 +1,69 @@
+# :author: Maxime VINCENT
+
+using Cbc, JuMP
+
+# TODO: Encapsuler dans une fonction
+
+# Disponibilités des nageurs de 13h00 à 18h30
+A = [
+    1 1 0 1 0 0 0 0 1 1 1 1; # Nageur 1
+    1 1 0 0 1 1 1 0 0 1 1 1;
+    1 1 0 0 0 0 1 1 1 0 0 1;
+    1 1 1 1 1 1 1 1 1 1 1 1;
+    1 1 0 1 0 1 1 1 1 0 1 1;
+    1 1 0 0 0 0 0 0 0 0 0 0;
+    0 1 1 1 1 0 0 0 1 0 0 0;
+    0 1 1 1 1 1 1 0 1 1 1 1;
+    0 1 0 1 0 0 1 0 1 1 1 1;
+    0 1 1 0 1 0 1 0 1 1 1 1;
+    0 1 1 1 1 1 0 0 0 1 1 1;
+    0 0 0 1 1 1 1 1 0 0 1 1; # Nageur 12
+]
+
+n_swims = size(A, 1) # Nombre de nageurs
+n_slots = size(A, 2) # Nombre de créneaux
+max_slots = 1 # Nombre maximum de créneaux autorisé par nageur 
+
+m = JuMP.Model(Cbc.Optimizer)
+
+# Si x[i,j] = 1 alors le nageur i est inscrit au créneau j
+@variable(m, x[i in 1:n_swims, j in 1:n_slots], Bin)
+
+# Chaque nageur ne doit être inscrit que sur max_slots créneau(x) au plus
+@constraint(m, line[i in 1:n_swims], sum(x[i,j] for j in 1:n_slots) <= max_slots)
+
+# Un créneau ne peut être accordé au plus qu'à un seul nageur
+@constraint(m, column[j in 1:n_slots], sum(x[i,j] for i in 1:n_swims) <= 1)
+
+# Si le nageur i n'est pas disponible sur le créneau j 
+# on ne doit pas lui proposer ce créneau
+@constraint(m, available[i in 1:n_swims, j in 1:n_slots ; A[i,j] == 0], x[i,j] == 0)
+
+# Une équipe est composée au maximum de n_slots nageurs
+# puisqu'on doit avoir au plus un nageur par créneau
+# Contrainte redondante avec column conjuguée à la fonction objectif
+#@constraint(m, team, sum(x[i,j] for i in 1:n_swims, j in 1:n_slots) <= n_slots)
+
+# On cherche à maximiser le nombre de nageurs en lice car faire nager 
+# plusieurs fois un même nageur risque de lui faire perdre en performance
+# et plus on est de fous plus on rit !
+@objective(m, Max, sum(x[i,j] for i in 1:n_swims, j in 1:n_slots))
+
+MOI.set(m, MOI.Silent(), true)
+optimize!(m)
+is_optimal = termination_status(m) == MOI.OPTIMAL
+
+if is_optimal println("Le pb a été résolu à l'optimum")
+else println("Le pb n'a pas été résolu à l'optimum")
+end
+
+println("sum(x[i,j]) = ", round(Int, JuMP.objective_value(m)))
+
+for i in 1:n_swims
+    for j in 1:n_slots
+        if (round(Int, JuMP.value(x[i,j])) == 1)
+            println("Nageur ", i, " nage en position ", j)
+            break
+        end
+    end
+end
